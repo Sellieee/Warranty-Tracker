18c5c8723cbda2f8b16fda6a93a6e3690b588b1e
Selina Van <sellieee@hotmail.com>
2020-07-04 12:05:18 +1000

    added components with initial react code

---
+++ mern-warranty-tracker-app/src/Components/homeappliances.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class HomeAppliances extends Component {
+    render() {
+        return(
+            <div> 
+               <p>Home Appliances Warranties</p> 
+            </div>
+        );
+    }
+}
