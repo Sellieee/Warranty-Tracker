337e7ccd549746de3452679dad9d4595464c15a9
Selina Van <sellieee@hotmail.com>
2020-07-04 10:57:02 +1000

    added router and possible routes to app.js

--- mern-warranty-tracker-app/src/App.js
+++ mern-warranty-tracker-app/src/App.js
@@ -1,13 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return(
-       <div className="container">
-         <h2>MERN-Stack Warranty Tracker</h2>
-       </div> 
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Warranty Tracker</h2>
+        </div>
 
+        {/* These routes are for separate web pages. 
+        <Route path="/" exact component={WarrantySummary} />
+        <Route path="/edit/:id" exact component={EditWarranties} />
+        <Route path="/create" exact component={CreateWarranties} />
+        */}
+        {/* Overview of existing Warranties */}
+        <Route path="/" exact component={WarrantySummary} />
+        {/* Overview of existing Warranties */}
+        <Route path="/electronics" exact component={WarrantyElectronics} />
+        {/* Overview of existing Warranties */}
+        <Route path="/car" exact component={WarrantyCar} />
+        {/* Overview of existing Warranties */}
+        <Route path="/homeappliances" exact component={WarrantyHomeAppliances} />
+      </Router>
     );
   }
 }
